f8fac4b9031e72caa0d55f768ebda8c4e9722b33
IceArrow256 <icearrow256@gmail.com>
2020-10-19 15:33:22 +0300

    fix header & create footer

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,12 @@ import django.contrib.auth.views as DCAV
 import app.views as views
 
 urlpatterns = [
+    DU.path('games', views.games, name='games'),
     DU.path('', views.index, name='index'),
+    DU.path('login', views.login, name='login'),
+    DU.path('my-games', views.my_games, name='my-games'),
+    DU.path('profile', views.profile, name='profile'),
+    DU.path('sign-up', views.sign_up, name='sign-up'),
     DU.path("logout/", DCAV.LogoutView.as_view(), name="logout"),
 ]
 
--- app/views.py
+++ app/views.py
@@ -2,10 +2,49 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+def games(request):
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    context = {'active': 'games', 'username': username}
+    return render(request, 'app/games.html', context)
+
+
 def index(request):
     username = None
     if request.user.is_authenticated:
         username = request.user.username
-
     context = {'active': 'home', 'username': username}
     return render(request, 'app/index.html', context)
+
+def login(request):
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    context = {'active': 'login', 'username': username}
+    return render(request, 'app/login.html', context)
+
+
+def my_games(request):
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    context = {'active': 'my-games', 'username': username}
+    return render(request, 'app/my-games.html', context)
+
+def profile(request):
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    context = {'active': 'profile', 'username': username}
+    return render(request, 'app/profile.html', context)
+
+
+def sign_up(request):
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    context = {'active': 'sign-up', 'username': username}
+    return render(request, 'app/sign-up.html', context)
+
